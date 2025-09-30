d1315aef3456313bcfb57a175f3d565ac7788ece
Hung-497 <giahung092007@gmail.com>
2025-09-30 11:01:35 +0300

    Project

--- Intro.py
+++ Intro.py
@@ -1,11 +1,13 @@
-import colorama
 from art import *
 from pyfiglet import Figlet
 from colorama import init, Fore, Style, Back
 def show_intro():
     print(Fore.RED + Style.BRIGHT,end='')
-    lprint(length=90, height=1, char="*")
-    f = Figlet(width=100).renderText("Survival - Flight")
+    lprint(length=180, height=1, char="#")
+    f = Figlet(font='ansi_shadow', width=200).renderText("Survival - Flight \nBy : Group - 1")
     print(f)
-    lprint(length=90, height=1, char="*")
+    lprint(length=180, height=1, char="#")
     print(Style.RESET_ALL, end=' ')
+show_intro()
+
+
